8b5b179cb6334c7cfe738bbfd920873bdd18a24f
Art <dxem158@gmail.com>
2025-02-28 00:14:34 +0700

    Delete Dem_Kytu.py

--- Dem_Kytu.py
+++
@@ -1,7 +0,0 @@
-from collections import Counter
-def dem_kytu():
-    chuoi = input("Mời nhập chuỗi ký tự: ").lower()
-    tan_suat = Counter(chuoi)
-    for ky_tu in sorted(tan_suat):
-        print(f"'{ky_tu}': {tan_suat[ky_tu  ]} ")
-dem_kytu()
